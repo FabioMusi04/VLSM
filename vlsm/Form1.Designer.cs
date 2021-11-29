243d15df4c4534c5174db962beac173b7aaa6c39
FabioMusi04 <fabio04musitelli@gmail.com>
2021-11-29 20:41:21 +0100

    Vlsm c#
    Vlsm c#

---
+++ vlsm/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace vlsm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnCalc = new System.Windows.Forms.Button();
+            this.txtNomSot = new System.Windows.Forms.TextBox();
+            this.TxtNumSot = new System.Windows.Forms.TextBox();
+            this.BtnInsSot = new System.Windows.Forms.Button();
+            this.lbSottoreti = new System.Windows.Forms.ListBox();
+            this.BtnVisuaSot = new System.Windows.Forms.Button();
+            this.BtnModif = new System.Windows.Forms.Button();
+            this.BtnEli = new System.Windows.Forms.Button();
+            this.groupBoxSot = new System.Windows.Forms.GroupBox();
+            this.dataGridViewReti = new System.Windows.Forms.DataGridView();
+            this.BtnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.groupBoxSot.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 54);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 32);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(85, 54);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(55, 32);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(162, 54);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(55, 32);
+            this.numericUpDown3.TabIndex = 2;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown4.Location = new System.Drawing.Point(243, 54);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(55, 32);
+            this.numericUpDown4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ip Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(221, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(144, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = ".";
+            // 
+            // BtnCalc
+            // 
+            this.BtnCalc.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnCalc.Location = new System.Drawing.Point(366, 57);
+            this.BtnCalc.Name = "BtnCalc";
+            this.BtnCalc.Size = new System.Drawing.Size(103, 37);
+            this.BtnCalc.TabIndex = 11;
+            this.BtnCalc.Text = "Calcola";
+            this.BtnCalc.UseVisualStyleBackColor = true;
+            this.BtnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // txtNomSot
+            // 
+            this.txtNomSot.Location = new System.Drawing.Point(68, 38);
+            this.txtNomSot.Name = "txtNomSot";
+            this.txtNomSot.Size = new System.Drawing.Size(100, 20);
+            this.txtNomSot.TabIndex = 12;
+            // 
+            // TxtNumSot
+            // 
+            this.TxtNumSot.Location = new System.Drawing.Point(206, 38);
+            this.TxtNumSot.Name = "TxtNumSot";
+            this.TxtNumSot.Size = new System.Drawing.Size(100, 20);
+            this.TxtNumSot.TabIndex = 13;
+            // 
+            // BtnInsSot
+            // 
+            this.BtnInsSot.Location = new System.Drawing.Point(53, 77);
+            this.BtnInsSot.Name = "BtnInsSot";
+            this.BtnInsSot.Size = new System.Drawing.Size(75, 23);
+            this.BtnInsSot.TabIndex = 14;
+            this.BtnInsSot.Text = "Inserisci";
+            this.BtnInsSot.UseVisualStyleBackColor = true;
+            this.BtnInsSot.Click += new System.EventHandler(this.BtnInsSot_Click);
+            // 
+            // lbSottoreti
+            // 
+            this.lbSottoreti.FormattingEnabled = true;
+            this.lbSottoreti.Location = new System.Drawing.Point(0, 111);
+            this.lbSottoreti.Name = "lbSottoreti";
+            this.lbSottoreti.Size = new System.Drawing.Size(120, 95);
+            this.lbSottoreti.TabIndex = 15;
+            this.lbSottoreti.SelectedIndexChanged += new System.EventHandler(this.lbSottoreti_SelectedIndexChanged);
+            // 
+            // BtnVisuaSot
+            // 
+            this.BtnVisuaSot.Location = new System.Drawing.Point(154, 183);
+            this.BtnVisuaSot.Name = "BtnVisuaSot";
+            this.BtnVisuaSot.Size = new System.Drawing.Size(75, 23);
+            this.BtnVisuaSot.TabIndex = 16;
+            this.BtnVisuaSot.Text = "Visualizza";
+            this.BtnVisuaSot.UseVisualStyleBackColor = true;
+            this.BtnVisuaSot.Click += new System.EventHandler(this.BtnVisuaSot_Click);
+            // 
+            // BtnModif
+            // 
+            this.BtnModif.Location = new System.Drawing.Point(150, 77);
+            this.BtnModif.Name = "BtnModif";
+            this.BtnModif.Size = new System.Drawing.Size(75, 23);
+            this.BtnModif.TabIndex = 17;
+            this.BtnModif.Text = "Modifica";
+            this.BtnModif.UseVisualStyleBackColor = true;
+            this.BtnModif.Click += new System.EventHandler(this.BtnModif_Click);
+            // 
+            // BtnEli
+            // 
+            this.BtnEli.Location = new System.Drawing.Point(242, 77);
+            this.BtnEli.Name = "BtnEli";
+            this.BtnEli.Size = new System.Drawing.Size(75, 23);
+            this.BtnEli.TabIndex = 18;
+            this.BtnEli.Text = "Elimina";
+            this.BtnEli.UseVisualStyleBackColor = true;
+            this.BtnEli.Click += new System.EventHandler(this.BtnEli_Click);
+            // 
+            // groupBoxSot
+            // 
+            this.groupBoxSot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.groupBoxSot.Controls.Add(this.txtNomSot);
+            this.groupBoxSot.Controls.Add(this.BtnEli);
+            this.groupBoxSot.Controls.Add(this.TxtNumSot);
+            this.groupBoxSot.Controls.Add(this.BtnModif);
+            this.groupBoxSot.Controls.Add(this.lbSottoreti);
+            this.groupBoxSot.Controls.Add(this.BtnInsSot);
+            this.groupBoxSot.Controls.Add(this.BtnVisuaSot);
+            this.groupBoxSot.Location = new System.Drawing.Point(1, 100);
+            this.groupBoxSot.Name = "groupBoxSot";
+            this.groupBoxSot.Size = new System.Drawing.Size(428, 206);
+            this.groupBoxSot.TabIndex = 19;
+            this.groupBoxSot.TabStop = false;
+            this.groupBoxSot.Text = "Sottoreti";
+            // 
+            // dataGridViewReti
+            // 
+            this.dataGridViewReti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewReti.Location = new System.Drawing.Point(435, 100);
+            this.dataGridViewReti.Name = "dataGridViewReti";
+            this.dataGridViewReti.Size = new System.Drawing.Size(364, 206);
+            this.dataGridViewReti.TabIndex = 20;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnReset.Location = new System.Drawing.Point(532, 57);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(107, 37);
+            this.BtnReset.TabIndex = 21;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 308);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.dataGridViewReti);
+            this.Controls.Add(this.groupBoxSot);
+            this.Controls.Add(this.BtnCalc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.groupBoxSot.ResumeLayout(false);
+            this.groupBoxSot.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReti)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnCalc;
+        private System.Windows.Forms.TextBox txtNomSot;
+        private System.Windows.Forms.TextBox TxtNumSot;
+        private System.Windows.Forms.Button BtnInsSot;
+        private System.Windows.Forms.ListBox lbSottoreti;
+        private System.Windows.Forms.Button BtnVisuaSot;
+        private System.Windows.Forms.Button BtnModif;
+        private System.Windows.Forms.Button BtnEli;
+        private System.Windows.Forms.GroupBox groupBoxSot;
+        private System.Windows.Forms.DataGridView dataGridViewReti;
+        private System.Windows.Forms.Button BtnReset;
+    }
+}
+
---
+++ vlsm/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+
+namespace vlsm
+{
+    public partial class Form1 : Form
+    {
+        private class Rete
+        {
+            public IPNetwork Address { get; set; }
+            public IPAddress Default_Gateway { get; set; }
+            public IPAddress Subnet { get; set; }
+            public IPAddress Primo_ip { get; set; }
+            public IPAddress Ultimo_ip { get; set; }
+
+            public Rete(IPNetwork Address)
+            {
+                this.Address = Address;
+                this.Default_Gateway = Address.LastUsable;
+                this.Subnet = Address.Netmask;
+                this.Primo_ip = Address.FirstUsable;
+                this.Ultimo_ip = Ultimoipusabile(this.Default_Gateway);
+            }
+
+            private IPAddress Ultimoipusabile(IPAddress Dg)
+            {
+                var ot = Dg.ToString().Split('.');
+                ot[3] = (int.Parse(ot[3]) - 1).ToString();                
+                return IPAddress.Parse($"{ot[0]}.{ot[1]}.{ot[2]}.{ot[3]}");
+            }
+        }
+        private class Sottoreti
+        {
+            public string SottoreteN { get; set; }
+            public int SottoreteNum { get; set; }
+
+            public Sottoreti(string n, int num)
+            {
+                if (string.IsNullOrEmpty(n))
+                    throw new Exception("E");
+                this.SottoreteN = n;
+                this.SottoreteNum = num;
+            }
+            public override string ToString()
+            {
+                return SottoreteN + "-" + SottoreteNum;
+            }
+        }
+
+        private IPAddress Ip_rete(IPAddress Broad)
+        {
+            var ot = Broad.ToString().Split('.');
+            if (int.Parse(ot[3]) < 255)
+                ot[3] = (int.Parse(ot[3]) + 1).ToString();
+            else if (int.Parse(ot[2]) < 255)
+                ot[2] = (int.Parse(ot[2]) + 1).ToString();
+            else if (int.Parse(ot[1]) < 255)
+                ot[1] = (int.Parse(ot[1]) + 1).ToString();
+            else
+                ot[0] = (int.Parse(ot[0]) + 1).ToString();
+            for(int t = 0; t < ot.Length; t++)
+                if (ot[t] == "255")
+                    ot[t] = "0";
+            return IPAddress.Parse($"{ot[0]}.{ot[1]}.{ot[2]}.{ot[3]}");
+        }
+
+        char findClass(char[] str)
+        {
+            // storing first octet in arr[] variable
+            char[] arr = new char[4];
+            int i = 0;
+            while (str[i] != '.')
+            {
+                arr[i] = str[i];
+                i++;
+            }
+            i--;
+
+            // converting str[] variable into number for
+            // comparison
+            int ip = 0, j = 1;
+            while (i >= 0)
+            {
+                ip = ip + (str[i] - '0') * j;
+                j = j * 10;
+                i--;
+            }
+
+            // Class A
+            if (ip >= 1 && ip <= 126)
+                return 'A';
+
+            // Class B
+            else if (ip >= 128 && ip <= 191)
+                return 'B';
+
+            // Class C
+            else if (ip >= 192 && ip <= 223)
+                return 'C';
+
+            // Class D
+            else if (ip >= 224 && ip <= 239)
+                return 'D';
+
+            // Class E       
+            else
+                return 'E';
+        }
+        public class Utilities
+        {
+            public static void ResetAllControls(Control form)
+            {
+                foreach (Control control in form.Controls)
+                {
+                    if (control is TextBox)
+                    {
+                        TextBox textBox = (TextBox)control;
+                        textBox.Text = null;
+                    }
+
+                    if (control is ListBox)
+                    {
+                        ListBox listBox = (ListBox)control;
+                        listBox.Items.Clear();
+                    }
+
+                    if (control is DataGridView)
+                    {
+                        DataGridView datagrid = (DataGridView)control;
+                        datagrid.DataSource = null;
+                        datagrid.Rows.Clear();
+                    }
+
+                    if (control is NumericUpDown)
+                    {
+                        NumericUpDown nume = (NumericUpDown)control;
+                        nume.Value = 0;
+                    }
+                }
+            }
+        }
+        bool CheckHost(char classe, int host)
+        {
+            if (classe == 'A')
+                if (host <= 16777214 && host > 65.534)
+                    return true;
+            if (classe == 'B')
+                if (host <= 65.534 && host > 254)
+                    return true;
+            if (classe == 'C')
+                if (host <= 254 && host > 0)
+                    return true;
+            if (classe == 'D')
+                return true;
+            if (classe == 'E')
+                return true;
+            return false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<Sottoreti> eleS = new List<Sottoreti>();
+        private List<Rete> eleR = new List<Rete>();
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            BtnCalc.Enabled = false;
+            BtnInsSot.Enabled = false;
+            BtnModif.Enabled = false;
+            BtnEli.Enabled = false;
+            IPAddress ipIns = IPAddress.Parse($"{numericUpDown1.Value}.{numericUpDown2.Value}.{numericUpDown3.Value}.{numericUpDown4.Value}");
+            char classe = findClass(ipIns.ToString().ToCharArray());
+
+            List<Sottoreti> eleSOrd = eleS.OrderByDescending(i => i.SottoreteNum).ToList();
+            lbSottoreti.Items.Clear();
+            foreach (var item in eleSOrd)
+            {
+                lbSottoreti.Items.Add(item);
+            }
+
+            var host = eleSOrd.Select(g => g.SottoreteNum);
+            int Host = 0;
+            foreach (var x in host)
+                Host = Host += x;
+
+            bool check = CheckHost(classe, Host);
+            if (check)
+            {
+                IPNetwork ip = default;
+                for (int i = 0; i < eleSOrd.Count(); i++)
+                {
+                    int next = int.Parse(Math.Ceiling(Math.Log(eleSOrd[i].SottoreteNum, 2)).ToString());
+                    Byte cidr = Byte.Parse((32 - next).ToString());
+                    
+                    if (i > 0)
+                    {
+                        ipIns = Ip_rete(ip.Broadcast);
+                        ip = IPNetwork.Parse($"{ipIns}/{cidr}");
+                        Rete r2 = new Rete(ip);
+                        eleR.Add(r2);
+                    }
+                    else
+                    {
+                        ip = IPNetwork.Parse($"{ipIns}/{cidr}");
+                        Rete r = new Rete(ip);
+                        eleR.Add(r);
+                    }
+                }
+                dataGridViewReti.DataSource = eleR;
+            }
+            else
+            {
+                MessageBox.Show("Non valido");
+                return;
+            }
+        }
+
+
+        private void BtnInsSot_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNomSot.Text) || int.TryParse(TxtNumSot.Text, out int Num))
+            {
+                Sottoreti s = new Sottoreti(txtNomSot.Text, int.Parse(TxtNumSot.Text) + 3);
+                eleS.Add(s);
+                MessageBox.Show("Inserita");
+            }
+            BtnVisuaSot.PerformClick();
+        }
+
+        private void BtnVisuaSot_Click(object sender, EventArgs e)
+        {
+            lbSottoreti.Items.Clear();
+            foreach(var item in eleS)
+            {
+                lbSottoreti.Items.Add(item);
+            }
+        }
+
+        private void lbSottoreti_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbSottoreti.SelectedIndex < 0)
+                return;
+            Sottoreti sotselez = lbSottoreti.SelectedItem as Sottoreti;
+            txtNomSot.Text = sotselez.SottoreteN;
+            TxtNumSot.Text = sotselez.SottoreteNum.ToString();
+        }
+
+        private void BtnModif_Click(object sender, EventArgs e)
+        {
+            if (lbSottoreti.SelectedIndex < 0)
+                return;
+            if (string.IsNullOrEmpty(txtNomSot.Text) || string.IsNullOrEmpty(TxtNumSot.Text))
+                MessageBox.Show("Inserisci qualcosa");
+            var eleP = eleS.Where(i => i.SottoreteN == txtNomSot.Text).FirstOrDefault();
+            if (eleP == null)
+                return;
+            if (int.TryParse(TxtNumSot.Text, out int Num))
+            {
+                eleP.SottoreteN = txtNomSot.Text;
+                eleP.SottoreteNum = Num;
+                MessageBox.Show("Modificato");
+            }
+            BtnVisuaSot.PerformClick();
+        }
+
+        private void BtnEli_Click(object sender, EventArgs e)
+        {
+            Sottoreti eleSelez = lbSottoreti.SelectedItem as Sottoreti;
+            eleS.RemoveAll(i => i.SottoreteN == eleSelez.SottoreteN);
+            BtnVisuaSot.PerformClick();
+            MessageBox.Show("Eliminato");
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Utilities.ResetAllControls(this);
+            Utilities.ResetAllControls(this.groupBoxSot);
+            eleR.Clear();
+            eleS.Clear();
+            BtnCalc.Enabled = true;
+            BtnInsSot.Enabled = true;
+            BtnModif.Enabled = true;
+            BtnEli.Enabled = true;
+        }
+    }
+}
